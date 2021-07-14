0dc917679e7ba4cff3c67c9f0fae81f04ef34997
Mohit182 <mohitguptatech18@gmail.com>
2021-07-14 14:39:34 +0530

    Added a list of items

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<transaction> transactions = [
+    transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.69,
+      date: DateTime.now(),
+    ),
+    transaction(
+      id: 't2',
+      title: 'Groceries',
+      amount: 45.45,
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,25 +28,72 @@ class MyApp extends StatelessWidget {
           title: Text('Expense Planner'),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Card(
               color: Colors.green,
-              elevation: 10,
-              margin: EdgeInsets.all(100.0),
+              elevation: 5,
               child: Container(
                 child: Text(
                   'Chart!',
+                  textScaleFactor: 1.5,
                   textAlign: TextAlign.center,
-                  textScaleFactor: 2,
                 ),
                 width: double.infinity,
               ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tx.amount.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
             Card(
               child: Container(
                 child: Text(
                   'List of txn!',
-                  textScaleFactor: 2,
+                  textScaleFactor: 1.5,
                   textAlign: TextAlign.center,
                 ),
                 width: double.infinity,
---
+++ lib/transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class transaction {
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+
+  transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+}
